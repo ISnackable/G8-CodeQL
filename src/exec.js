2dedecd80f3fc88871c12ee6fe92c4644fad4026
Tommy <52971804+ISnackable@users.noreply.github.com>
2021-03-16 16:44:33 +0800

    Add Expressjs Command Injection Vulnerability

--- src/exec.js
+++ src/exec.js
@@ -1,13 +1,41 @@
-const { exec } = require("child_process");
+"use strict";
 
-exec("ls -la", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
+const express = require('express');
+const { exec } = require("child_process");
+const app = express();
+
+app.post("/ping/:input", (req, res) => {
+    var input = req.params.input;
+
+    if (input) {
+        exec(`ping ${input}`, (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                res.status(200).send(error.message);
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                res.status(200).send(stderr);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+            res.status(200).send(stdout);
+        });
         return;
     }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
+    return res.status(400).send("bad");
 });
+
+function startServer() {
+    try {
+        app.listen(3000, "localhost", () => {
+            console.log(`Server started and accessible via http://localhost:3000/`);
+        });
+    }
+    catch(err) {
+        console.log(err);
+    }
+}
+
+startServer();

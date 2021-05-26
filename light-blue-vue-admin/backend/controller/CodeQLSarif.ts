177fa0ec63c83af16e4d7b5967b4d7b749afca2f
Tommy <52971804+ISnackable@users.noreply.github.com>
2021-05-26 18:31:06 +0800

    Add code for parsing sarif, and connection to Neo4J
    added bunch of sarifTestFiles too

---
+++ light-blue-vue-admin/backend/controller/CodeQLSarif.ts
@@ -0,0 +1,66 @@
+import { SarifLog } from "./CodeQLSarifValidator"
+var jsonMap = require('json-source-map');
+import * as fs from 'fs';
+
+const file = fs.readFileSync("./sarifTestFiles/RMerl_asuswrt-merlin__2021-05-20_17_18_36__export.sarif", 'utf8');
+// const file = fs.readFileSync("./sarifTestFiles/tplink1.sarif", 'utf8');
+
+// console.log(file);
+const testSarifJson: SarifLog = jsonMap.parse(file).data;
+
+// console.log(testSarifJson.runs);
+
+
+let results = testSarifJson.runs[0].results;
+let driverRules = testSarifJson.runs[0].tool.driver.rules;
+
+// https://betterprogramming.pub/which-is-the-fastest-while-for-foreach-for-of-9022902be15e
+// console.time('loop');
+// for (let i=0; i<results.length; i++){
+//     console.log("================================================================================================");
+//     console.log(`> ruleId: ${results[i].ruleId}`);
+//     console.log(`> message.text: ${results[i].message.text}`);
+//     console.log(`> locations[0].physicalLocation.artifactLocation.uri: ${results[i].locations[0].physicalLocation.artifactLocation.uri}`);
+//     console.log(`> locations[0].physicalLocation.region: ${JSON.stringify( results[i].locations[0].physicalLocation.region )}`);
+//     console.log(`> locations[0].physicalLocation.contextRegion?.snippet?.text: \n${results[i].locations[0].physicalLocation.contextRegion?.snippet?.text}`);
+//     console.log(`> codeFlows?.[0].threadFlows[0].locations: ${results[i].codeFlows?.[0].threadFlows[0].locations ?? "NOTHING"}`);
+//     console.log("================================================================================================\n");
+// }
+// console.timeEnd('loop');
+
+var noOfError = 0, noOfWarnings= 0, noOfRecommendation = 0;
+
+console.time('forofloop');
+for (const result of results){
+    let index = driverRules.findIndex(x => x.id === result.ruleId);
+    if (driverRules[index].properties["problem.severity"] === "error") {
+        noOfError++;
+    } else if (driverRules[index].properties["problem.severity"] === "warning") {
+        noOfWarnings++;
+    } else if (driverRules[index].properties["problem.severity"] === "recommendation") {
+        noOfRecommendation++;
+    }
+
+    console.log("<<==============================++++   QUERY INFO   +++======================================>>");
+    console.log(`> queryName: ${driverRules[index].properties.name}`);
+    console.log(`> queryTags: ${driverRules[index].properties.tags}`);
+    console.log(`> queryDescription: ${driverRules[index].fullDescription.text}`);
+    console.log(`> querySeverity: ${driverRules[index].properties["problem.severity"]}`);
+
+    console.log("\n-----------------------------------  RESULT INFO ----------------------------------------------------");
+    console.log(`> ruleId: ${result.ruleId}`);
+    console.log(`> message.text: ${result.message.text}`);
+    console.log(`> locations[0].physicalLocation.artifactLocation.uri: ${result.locations[0].physicalLocation.artifactLocation.uri}`);
+    console.log(`> locations[0].physicalLocation.region: ${JSON.stringify( result.locations[0].physicalLocation.region )}`);
+    console.log(`> locations[0].physicalLocation.contextRegion?.snippet?.text: \n${result.locations[0].physicalLocation.contextRegion?.snippet?.text}`);
+    console.log(`> result.codeFlows?.[0].threadFlows[0].locations: ${result.codeFlows?.[0].threadFlows[0].locations ?? "NOTHING"}`);
+    
+    console.log("<<==============================================================================================>>\n");
+
+}
+console.timeEnd('forofloop');
+
+console.log(`number of alerts: ${results.length}`);
+console.log(`number of errors: ${noOfError}`);
+console.log(`number of warning: ${noOfWarnings}`);
+console.log(`number of recommendation: ${noOfRecommendation}`);
--- light-blue-vue-admin/backend/model/databaseConfig.js
+++ light-blue-vue-admin/backend/model/databaseConfig.js
@@ -7,34 +7,73 @@ console.log("-----------------------------------------");
 // ---------------------------------------------------------
 // load modules
 // ---------------------------------------------------------
+const neo4j = require('neo4j-driver');
 
 // Neo4J Database
-const neo4j = require('neo4j-driver')
-
-const driver = neo4j.driver(uri, neo4j.auth.basic(user, password))
+const driver = neo4j.driver('bolt://localhost:7687',
+neo4j.auth.basic('REDACTED', 'REDACTED'), 
+{/* encrypted: 'ENCRYPTION_OFF' */});
 const session = driver.session()
-const personName = 'Alice'
 
-try {
-  const result = await session.run(
-    'CREATE (a:Person {name: $name}) RETURN a',
-    { name: personName }
-  )
+const query =
+`
+// SHOW DATABASES
+CREATE DATABASE SOMEHARDCODEDDATABASEFORNOW
+:USE SOMEHARDCODEDDATABASEFORNOW
 
-  const singleRecord = result.records[0]
-  const node = singleRecord.get(0)
+CREATE (XSSDOM:Query {born:'XSS'}) // QERY
 
-  console.log(node.properties.name)
-} finally {
-  await session.close()
-}
+CREATE (A1:Alert {born:"Alert 1"}) // ALERT 1
+CREATE (A2:Alert {born:"Alert 2"}) // ALERT 2
 
-// on application exit:
-await driver.close()
+CREATE (V1:Vulnerability {born:"code1"}) // Vulnerability 1
+CREATE (V2:Vulnerability {born:"code2"}) // Vulnerability 2
+CREATE (V3:Vulnerability {born:"code3"}) // Vulnerability 3
 
+CREATE
+(A1)-[:Child {roles:['Path1']}]->(XSSDOM),
+(A2)-[:Child {roles:['Path1']}]->(XSSDOM),
 
-// ---------------------------------------------------------
-// export
-// ---------------------------------------------------------
-module.exports = conn;
+(V1)-[:Child]->(A1),
+(V2)-[:Child]->(A2),
+(V3)-[:Child]->(V2)
+
+WITH XSSDOM as q
+MATCH (q)<-[:Child*]-(a)<-[:Child*]-(v) RETURN q,a, v;
+`;
+const params = { name: "Alice" };
+
+session
+  .run(query, params)
+  .then(result => {
+    result.records.forEach(record => {
+      console.log(record.get("a"));
+      console.log(JSON.stringify(record));
+      console.log("=======================");
+    })
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  .finally(() => {
+    session.close();
+    driver.close();
+  })
+
+// try {
+// const result = await session.run(query, params);
+
+// console.log(result);
+// console.log(JSON.stringify(result));
+// const singleRecord = result.records[0];
+// const node = singleRecord.get(0);
+
+// console.log(node.properties.name);
+// } 
+// finally {
+// await session.close();
+// }
+
+// // on application exit:
+// await driver.close()
 

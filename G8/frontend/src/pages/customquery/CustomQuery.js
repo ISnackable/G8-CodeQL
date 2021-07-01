5a36b2057eca4c7ce9ff980a3a7ce6faaf5ef248
hackedbyskittles <hackedbyskittles@gmail.com>
2021-07-01 17:17:34 +0800

    junyan continue

--- G8/frontend/src/pages/customquery/CustomQuery.js
+++ G8/frontend/src/pages/customquery/CustomQuery.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import TextareaAutosize from 'react-textarea-autosize';
 import AceEditor from 'react-ace'
@@ -34,6 +34,8 @@ import {
   faFolder,
   faQuestion,
   faArrowRight,
+  faCaretDown,
+  
 
 } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
@@ -63,29 +65,227 @@ import { ExistingProjectTable } from "../../components/Tables";
 // import { trafficShares } from "../../data/charts";
 // import Code from "../../components/Code";
 
+// var EditSession = require("ace/edit_session").EditSession;
+// var js = new EditSession("some js code");
+// var css = new EditSession(["some", "css", "code here"]);
+// // and then to load document into editor, just call
+// editor.setSession(js);
+
+
+// onclick handler will call the setcode function in line 77 and replace "code" variable with the "setCode" parameter contents
 
 const CustomQuery = () => {
+  let [responseData, setResponseData] = React.useState([]);
+  let [currentProject, setCurrentProject] = React.useState({id:null});
   const [code, setCode] = useState(`import javascript
-
 from BlockStmt b
 where b.getNumStmt() = 0
 select b, "This is an empty block."`)
+const fetchData = (e) => {
+  const backend_url = `http://localhost:8080/teamname/api`;
+  if (e) e.preventDefault();
+  axios
+    .get(backend_url + `/projects`)
+    .then((response) => {
+      console.log(response.data);
+      setResponseData(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+useEffect(() => {
+  fetchData();
+  return () => {};
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+}, []);
+
+const DrawProjectlist=()=>{
+  return responseData.map((pv)=>{
+    let {id, project_name, hash, sarif_filename, created_at} = pv
+    return (
+      <>
+    <Dropdown.Item className="fw-bold" onClick={()=>setCurrentProject({id:id})}><span>{project_name}</span><span> #{id}</span></Dropdown.Item>
+      </>
+    )
+  })
+}
+
+const sendCustomQuery=()=>{
+  axios.get(backend_url+`/test/`+currentProject+`/`)//????
+  .then((response)=>{
+    console.log(response)
+  })
+}
+
+const SampleQueries=() => {
+  return(
+    <>
+  <Dropdown.Item className="fw-bold" 
+  onClick={() => {
+    setCode(`import javascript
+import DataFlow::PathGraph
+import semmle.javascript.security.dataflow.LogInjection::LogInjection
+
+from LogInjectionConfiguration config, DataFlow::PathNode source, DataFlow::PathNode sink
+where config.hasFlowPath(source, sink)
+select sink.getNode(), source, sink, "$@ flows to log entry.", source.getNode(),"User-provided value"
+
+    
+    `)
+  }}>
+  <span>Log Injection</span>
+
+
+  </Dropdown.Item>
+  
+  <Dropdown.Item className="fw-bold" 
+  onClick={() => {
+    setCode(`import javascript
+import semmle.javascript.security.dataflow.StoredXss::StoredXss  
+import DataFlow::PathGraph
+    
+from Configuration cfg, DataFlow::PathNode source, DataFlow::PathNode sink
+where cfg.hasFlowPath(source, sink)
+select sink.getNode(), source, sink, "Stored cross-site scripting vulnerability due to $@.",
+source.getNode(), "stored value"
+
+    
+    `)
+  }}>
+
+<span>Stored XSS </span>
+
+
+  </Dropdown.Item>
+
+  <Dropdown.Item className="fw-bold" 
+  onClick={() => {
+    setCode(`import javascript
+import semmle.javascript.security.dataflow.CodeInjection::CodeInjection
+import DataFlow::PathGraph
+    
+from Configuration cfg, DataFlow::PathNode source, DataFlow::PathNode sink
+where cfg.hasFlowPath(source, sink)
+select sink.getNode(), source, sink,
+"$@ flows to " + sink.getNode().(Sink).getMessageSuffix() + ".", source.getNode(),
+"User-provided value"
+    
+    
+    `)
+  }}>
+
+<span> Code Injection </span>
+
+
+  </Dropdown.Item>
+  
+  <Dropdown.Item className="fw-bold" 
+  onClick={() => {
+    setCode(`import javascript
+import semmle.javascript.security.dataflow.RegExpInjection::RegExpInjection
+import DataFlow::PathGraph
+    
+from Configuration cfg, DataFlow::PathNode source, DataFlow::PathNode sink
+where cfg.hasFlowPath(source, sink)
+select sink.getNode(), source, sink, "This regular expression is constructed from a $@.",
+source.getNode(), "user-provided value"
+  
+    
+    `)
+  }}>
+
+<span> Regular expression Injection </span>
+
+
+  </Dropdown.Item>
+
+  <Dropdown.Item className="fw-bold" 
+  onClick={() => {
+    setCode(`import javascript
+import semmle.javascript.security.dataflow.CleartextStorage::CleartextStorage
+import DataFlow::PathGraph
+
+from Configuration cfg, DataFlow::PathNode source, DataFlow::PathNode sink
+where cfg.hasFlowPath(source, sink)
+select sink.getNode(), source, sink, "Sensitive data returned by $@ is stored here.",
+source.getNode(), source.getNode().(Source).describe()
+    
+    `)
+  }}>
+
+<span> Clear Text Storage </span>
+
+
+  </Dropdown.Item>
+
+  <Dropdown.Item className="fw-bold" 
+  onClick={() => {
+    setCode(`import javascript
+
+    from InvokeExpr invk, DataFlow::FunctionNode f
+    where f.flowsToExpr(invk.getAnArgument())
+    select invk, f
+    
+    
+    `)
+  }}>
+
+<span> Callback Query </span>
+
+
+  </Dropdown.Item>
+
+
+</>
+  )
+
+
+}
+
 
   
     return (
+      
       <Card>
       <Card.Body>
+      
+      <div style={{ float : 'right',  marginRight : "50px" , marginTop : "30px"}}>
+          <Dropdown className="btn-toolbar">
+            <Dropdown.Toggle
+              as={Button}
+              variant="primary"
+              size="lg"
+              className="me-2"
+              style={{backgroundColor:"red"}}
+            >
+              Choose Project 
+              <FontAwesomeIcon icon={faCaretDown}  className="me-2 mx-2" />
+            </Dropdown.Toggle>
+            <Dropdown.Menu className="dashboard-dropdown dropdown-menu-left mt-2">
+              <DrawProjectlist />
+              <Dropdown.Divider>
+                {/* Sample project goes here */}
+              </Dropdown.Divider>
+            </Dropdown.Menu>
+          </Dropdown>
+  
+        </div>
+      
         <article>
-          <h1 className="h2" id="quick-start">
+          <h1 className="h2" id="quick-start" style={{ marginLeft : '20px ', marginTop : '10px'}}>
             Custom Query{" "}
           </h1>
-          <p className="fs-5 fw-light">
+          <p className="fs-5 fw-light" style={{ marginLeft : '20px'}}>
             Create your own queries and run them against your own projects !
           </p>
 
           
 
         </article>
+
+
+      
       
       <Row className="justify-content-md-center">
 
@@ -94,7 +294,7 @@ select b, "This is an empty block."`)
         <div className = "w-100 p-3">
           {/* something about on clicks here  */}
 
-        <label> Type in the box below </label>
+        <label style={{ marginBottom : '10px'}}> Type in the box below </label>
 
         <AceEditor style={{
                 height: '50vh',
@@ -123,41 +323,46 @@ select b, "This is an empty block."`)
             }} />
         </div>
 
-        <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
+
+        <div style={{ float : 'left',  marginRight : "50px", marginLeft: '20px'}}>
           <Dropdown className="btn-toolbar">
             <Dropdown.Toggle
               as={Button}
               variant="primary"
-              size="sm"
+              size="lg"
               className="me-2"
-              style={{backgroundColor:"Green"}}
+              style={{backgroundColor:"#262b40"}}
             >
-              <FontAwesomeIcon icon={faArrowRight}  className="me-2" />
-              Run Query 
+              Click for some sample queries
+              <FontAwesomeIcon icon={faCaretDown}  className="me-2 mx-2" />
             </Dropdown.Toggle>
             <Dropdown.Menu className="dashboard-dropdown dropdown-menu-left mt-2">
-              <Dropdown.Item className="fw-bold">
-                <FontAwesomeIcon icon={faCloudUploadAlt} className="me-2" />{" "}
-                Scan against my projects
-              </Dropdown.Item>
-              <Dropdown.Item className="fw-bold">
-                <FontAwesomeIcon icon={faUserShield} className="me-2" /> Scan against Sample Projects
-              </Dropdown.Item>
-  
-              <Dropdown.Divider />
-  
-            
+              <SampleQueries />
+              <Dropdown.Divider>
+                {/* Sample project goes here */}
+              </Dropdown.Divider>
             </Dropdown.Menu>
           </Dropdown>
-  
+          </div>
+
+        <div style={{ float : 'right',  marginRight : "50px"}}> 
+          <Button 
+          variant = "Success" 
+          size="lg"
+          style={{backgroundColor:"green" , color:'black'}}> 
+          Run Query 
+          
+          </Button>
         </div>
   
        </Col>
   
       </Row>
+      
+      
       </Card.Body>
       </Card>
-
+    
       
     );
 
